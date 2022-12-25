006f84a812bd3ca6ce78f5172b88457ec909a3e9
Josh Levy <>
2022-12-25 17:28:49 -0600

    progress save

--- crack.py
+++ crack.py
@@ -5,19 +5,42 @@ import string
 
 pg.PAUSE = 0.000001
 
-for i in range(5, 0, -1):
-    print("Starting in", i)
-    sleep(1)
-print("Let's get cracking ...\n")
 
-chars = string.ascii_lowercase + string.ascii_uppercase + string.digits + " "
-low, high = 1, 6
-for password_length in range(low, high):
-    lengths = [37, 1_369, 50_653, 1_874_161, 69_343_957]
-    count = 1
-    for guess in product(chars, repeat=password_length):
-        print(f"{count}/{lengths[password_length]} - {''.join(guess)}")
-        count += 1
-        guess = ''.join(guess)
-        pg.write(guess)
-        pg.press('enter')
+def countdown(start=5):
+    for i in range(start, 0, -1):
+        print("Starting in", i)
+        sleep(1)
+    print("Let's get cracking ...\n")
+
+
+def get_progress():
+    with open("progress.txt", "r") as f:
+        n, progress = f.readline().split()
+        return int(n), int(progress)
+
+
+def write_progress(n, progress):
+    with open("progress.txt", "w") as f:
+        f.write(f"{n} {progress}")
+
+
+def crack_number(n, progress=1):
+    chars = string.ascii_lowercase + string.digits + " "
+    guesses = list(product(chars, repeat=n))
+    n_guesses = len(guesses) - 1
+    countdown()
+    for guess in guesses[progress:]:
+        try:
+            guess = ''.join(guess)
+            print(f"{progress}/{n_guesses} - {guess}")
+            progress += 1
+            pg.write(guess)
+            pg.press('enter')
+        except KeyboardInterrupt:
+            print("Keyboard interrupt")
+            write_progress(n, progress - 1)
+            break
+
+
+n, progress = get_progress()
+crack_number(n, progress)
